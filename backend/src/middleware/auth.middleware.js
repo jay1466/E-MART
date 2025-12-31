6e952ca3487c027096b3b5a867c9e926b529929f
Jay <thesiyajay54@gmail.com>
2025-12-31 14:21:44 +0530

    Middleware Added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+//here two middleware 1st is for user logged in or not
+//2nd is logged in user owner or not 
+import jwt from "jsonwebtoken"
+import { ENV } from "../config/env.js"
+
+export const protectRoute = async(req, res, next)=>{
+    try {
+        const token = req.cookies.token
+
+        if(!token){
+            return res.status(401).json({
+                message:"Token not found"
+            })
+        }
+
+        const decode = await jwt.verify(token, ENV.JWT_TOKEN) 
+
+        if(!decode){
+            return res.status(401).json({
+                message:"User not logged in"
+            })
+        }
+
+        req.id = decode.userId
+
+        next()
+
+    } catch (error) {
+        console.log(`error from protect middlerware, ${error}`)
+    }
+}
+
